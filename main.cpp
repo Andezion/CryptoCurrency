53127b217226be25e0e4ed482a2a7be86b8d5701
Andezion <245122@edu.p.lodz.pl>
2025-10-01 00:04:46 +0200

    added basic structure

--- main.cpp
+++ main.cpp
@@ -4,17 +4,68 @@
 #include <nlohmann/json.hpp>
 #include <wx/wx.h>
 
-class MyApp : public wxApp {
+class MyApp : public wxApp
+{
 public:
-    bool OnInit() override {
-        auto *frame = new wxFrame(nullptr, wxID_ANY, "Hello wxWidgets");
-        frame->Show(true);
-        return true;
-    }
+    virtual bool OnInit();
 };
 
-wxIMPLEMENT_APP(MyApp);
+class MyFrame : public wxFrame
+{
+public:
+    MyFrame(const wxString& title);
+    void OnQuit(wxCommandEvent& event);
+    void OnAbout(wxCommandEvent& event);
+private:
+    DECLARE_EVENT_TABLE()
+};
 
+DECLARE_APP(MyApp)
+IMPLEMENT_APP(MyApp)
+
+bool MyApp::OnInit()
+{
+    MyFrame *frame = new MyFrame(wxT("Minimal wxWidgets App"));
+    frame->Show(true);
+
+    return true;
+}
+
+BEGIN_EVENT_TABLE(MyFrame, wxFrame)
+EVT_MENU(wxID_ABOUT, MyFrame::OnAbout)
+EVT_MENU(wxID_EXIT, MyFrame::OnQuit)
+END_EVENT_TABLE()
+
+void MyFrame::OnAbout(wxCommandEvent& event)
+{
+    wxString msg;
+    msg.Printf(wxT("Hello and welcome to %s"),
+    wxVERSION_STRING);
+    wxMessageBox(msg, wxT("About Minimal"),
+    wxOK | wxICON_INFORMATION, this);
+}
+void MyFrame::OnQuit(wxCommandEvent& event)
+{
+    Close();
+}
+
+MyFrame::MyFrame(const wxString& title)
+: wxFrame(NULL, wxID_ANY, title)
+{
+    wxMenu *fileMenu = new wxMenu;
+
+    wxMenu *helpMenu = new wxMenu;
+    helpMenu->Append(wxID_ABOUT, wxT("&About...\tF1"),
+    wxT("Show about dialog"));
+    fileMenu->Append(wxID_EXIT, wxT("E&xit\tAlt-X"),
+    wxT("Quit this program"));
+    wxMenuBar *menuBar = new wxMenuBar();
+    menuBar->Append(fileMenu, wxT("&File"));
+    menuBar->Append(helpMenu, wxT("&Help"));
+    SetMenuBar(menuBar);
+    CreateStatusBar(2);
+    SetStatusText(wxT("Welcome to wxWidgets!"));
+}
 // using json = nlohmann::json;
 //
 // size_t WriteCallback(void* contents, const size_t size, const size_t nmemb, std::string* output) {
